c4e24b34139431a0b95a478bca51d853279383fb
lyw3447 <lyw3447@naver.com>
2019-05-26 01:20:00 +0900

    Course implement

--- src/main/java/edu/handong/analysis/datamodel/Course.java
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -1,5 +1,7 @@
 package edu.handong.analysis.datamodel;
 
+import java.util.ArrayList;
+
 public class Course {
 	private String studentId;
 	private String yearMonthGraduated;
@@ -12,6 +14,22 @@ public class Course {
 	private int semesterCourseTaken;
 
 	public Course(String line) {
+		ArrayList<String> courseList = new ArrayList<String>();
 		
+		String[] split = line.trim().split(", ");
+		for (String el : split) {
+			courseList.add(el);
+		}
+		
+		studentId = courseList.get(0);
+		yearMonthGraduated = courseList.get(1);
+		firstMajor = courseList.get(2);
+		secondMajor = courseList.get(3);
+		courseCode = courseList.get(4);
+		courseName = courseList.get(5);
+		courseCredit = courseList.get(6);
+		yearTaken = Integer.parseInt(courseList.get(7));
+		semesterCourseTaken = Integer.parseInt(courseList.get(8));
 	}
+
 }
