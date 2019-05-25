d85089a4484dd11dd194ef8f003ebf66eb6d639b
lyw3447 <lyw3447@naver.com>
2019-05-26 02:12:15 +0900

    implement NotEnoughArgumentException

--- src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
+++ src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
@@ -1,11 +1,11 @@
 package edu.handong.analysis.utils;
 
-public class NotEnoughArgumentException {
+public class NotEnoughArgumentException extends Exception{
 	public NotEnoughArgumentException() {
-		
+		super("Not Enough Argument! Please try again..\n");
 	}
 	public NotEnoughArgumentException(String message) {
-		
+		super(message);
 	}
 
 }
