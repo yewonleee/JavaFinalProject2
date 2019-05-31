2ca54e9ac52b78b9f03f0da26b641dbd9a983f12
lyw3447 <lyw3447@naver.com>
2019-05-31 23:18:05 +0900

    change to CSVRecord

--- src/main/java/edu/handong/analysis/datamodel/Course.java
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -1,6 +1,6 @@
 package edu.handong.analysis.datamodel;
 
-import java.util.ArrayList;
+import org.apache.commons.csv.CSVRecord;
 
 public class Course {
 	private String studentId;
@@ -13,31 +13,89 @@ public class Course {
 	private int yearTaken;
 	private int semesterCourseTaken;
 
-	public Course(String line) {
-		ArrayList<String> courseList = new ArrayList<String>();
+	public Course(CSVRecord item) {
 		
-		String[] split = line.trim().split(", ");
-		for (String el : split) {
-			courseList.add(el);
-		}
-		
-		studentId = courseList.get(0);
-		yearMonthGraduated = courseList.get(1);
-		firstMajor = courseList.get(2);
-		secondMajor = courseList.get(3);
-		courseCode = courseList.get(4);
-		courseName = courseList.get(5);
-		courseCredit = courseList.get(6);
-		yearTaken = Integer.parseInt(courseList.get(7));
-		semesterCourseTaken = Integer.parseInt(courseList.get(8));
+		studentId = item.get(0);
+		yearMonthGraduated = item.get(1);
+		firstMajor = item.get(2);
+		secondMajor = item.get(3);
+		courseCode = item.get(4);
+		courseName = item.get(5);
+		courseCredit = item.get(6);
+		yearTaken = Integer.parseInt(item.get(7));
+		semesterCourseTaken = Integer.parseInt(item.get(8));
 	}
 	
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getYearMonthGraduated() {
+		return yearMonthGraduated;
+	}
+
+	public void setYearMonthGraduated(String yearMonthGraduated) {
+		this.yearMonthGraduated = yearMonthGraduated;
+	}
+
+	public String getFirstMajor() {
+		return firstMajor;
+	}
+
+	public void setFirstMajor(String firstMajor) {
+		this.firstMajor = firstMajor;
+	}
+
+	public String getSecondMajor() {
+		return secondMajor;
+	}
+
+	public void setSecondMajor(String secondMajor) {
+		this.secondMajor = secondMajor;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getCourseCredit() {
+		return courseCredit;
+	}
+
+	public void setCourseCredit(String courseCredit) {
+		this.courseCredit = courseCredit;
+	}
+
 	public int getYearTaken() {
 		return yearTaken;
 	}
-	
+
+	public void setYearTaken(int yearTaken) {
+		this.yearTaken = yearTaken;
+	}
+
 	public int getSemesterCourseTaken() {
 		return semesterCourseTaken;
+	}
+
+	public void setSemesterCourseTaken(int semesterCourseTaken) {
+		this.semesterCourseTaken = semesterCourseTaken;
 	}
 
 }
